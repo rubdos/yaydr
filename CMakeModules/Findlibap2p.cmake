3b8e531946b53895bcee44057ad8f652bd79afc9
Ruben De Smet <ruben.de.smet@telenet.be>
2012-11-01 21:18:52 +0100

    Throw error when not found ap2p

--- CMakeModules/Findlibap2p.cmake
+++ CMakeModules/Findlibap2p.cmake
@@ -1,14 +1,22 @@
 FIND_PATH(LIBAP2P_INCLUDEDIR NAMES libap2p/network/network.hpp
-    /usr/local/include
-    /usr/include
+    HINTS
+        /usr/local/include
+        /usr/include
 )
-FIND_LIBRARY(LIBAP2P_LIBS NAMES libap2p
-    /usr/local/lib
-    /usr/lib
+FIND_LIBRARY(LIBAP2P_LIBS NAMES ap2p
+    HINTS
+        /usr/local/lib
+        /usr/lib
 )
 IF(LIBAP2P_INCLUDEDIR AND LIBAP2P_LIBS)
     SET(LIBAP2P_INCLUDE_DIRS ${LIBAP2P_INCLUDEDIR})
     SET(LIBAP2P_FOUND TRUE)
+    MESSAGE(STATUS "Found libap2p")
 ELSE()
+    IF (libap2p_FIND_REQUIRED)
+        MESSAGE(FATAL_ERROR "Fatal: Could not find libap2p.")
+    ELSE ()
+        MESSAGE(STATUS "Not compiling with libap2p")
+    ENDIF()
     SET(LIBAP2P_FOUND FALSE)
 ENDIF()
