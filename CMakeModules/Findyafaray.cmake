ddef604c574611f9c991948a5472be0c4888c2ab
Ruben De Smet <ruben.de.smet@telenet.be>
2012-11-23 13:06:41 +0100

    Added Findyafaray.cmake

---
+++ CMakeModules/Findyafaray.cmake
@@ -0,0 +1,21 @@
+FIND_PATH(YAFARAY_INCLUDEDIR NAMES yafaray/interface/xmlinterface.h
+    /usr/local/include
+    /usr/include
+)
+FIND_LIBRARY(YAFARAY_LIBS NAMES libyafaraycore.so 
+        libyafarayplugin.so  
+        plugins  
+        yafrayinterface.py  
+        _yafrayinterface.so
+    HINTS
+        /usr/local/lib
+        /usr/lib
+)
+IF(YAFARAY_INCLUDEDIR AND YAFARAY_LIBS)
+    SET(YAFARAY_INCLUDE_DIRS ${YAFARAY_INCLUDEDIR})
+    MESSAGE(STATUS "Yafaray found")
+    SET(YAFARAY_FOUND TRUE)
+ELSE()
+    MESSAGE(FATAL_ERROR "Yafaray NOT found")
+    SET(YAFARAY_FOUND FALSE)
+ENDIF()
