c1ba68e7872371671322e16100ce2b0e43606d26
Ruben De Smet <ruben.de.smet@telenet.be>
2013-02-05 18:23:27 +0100

    Made Yafaray finding easier on Fedora Spherical Cow

--- CMakeModules/Findyafaray.cmake
+++ CMakeModules/Findyafaray.cmake
@@ -2,14 +2,15 @@ FIND_PATH(YAFARAY_INCLUDEDIR NAMES yafaray/interface/xmlinterface.h
     /usr/local/include
     /usr/include
 )
-FIND_LIBRARY(YAFARAY_LIBS NAMES libyafaraycore.so 
-        libyafarayplugin.so  
+FIND_LIBRARY(YAFARAY_LIBS NAMES libyafaraycore.so
+        libyafarayplugin.so
         plugins  
-        yafrayinterface.py  
         _yafrayinterface.so
     HINTS
         /usr/local/lib
+        /usr/local/bin
         /usr/lib
+        /usr/bin
 )
 IF(YAFARAY_INCLUDEDIR AND YAFARAY_LIBS)
     SET(YAFARAY_INCLUDE_DIRS ${YAFARAY_INCLUDEDIR})
