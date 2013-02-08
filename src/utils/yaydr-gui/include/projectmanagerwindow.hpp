e15fe6523813f49bfe8563c5cf43f4545697d06b
Ruben De Smet <ruben.de.smet@telenet.be>
2013-02-08 21:08:25 +0100

    Added scrollarea main widget and stuff

--- src/utils/yaydr-gui/include/projectmanagerwindow.hpp
+++ src/utils/yaydr-gui/include/projectmanagerwindow.hpp
@@ -8,11 +8,15 @@ public:
 private:
     void _createMenus();
     void _createTrayIcon();
+    void _createGrid();
 
-    QTextEdit* _text;
     QMenu* _fileMenu;
     QSystemTrayIcon* _trayIcon;
     QMenu* _trayIconMenu;
+
+    QScrollArea* _viewportScrollArea;
+    QWidget* _scrollWidget;
+    QVBoxLayout* _mainGrid;
 
 private slots:
     void quit();
--- src/utils/yaydr-gui/projectmanagerwindow.cpp
+++ src/utils/yaydr-gui/projectmanagerwindow.cpp
@@ -4,13 +4,31 @@ ProjectManagerWindow::ProjectManagerWindow()
 {
     this->_createMenus();
     this->_createTrayIcon();
-
-    this->_text = new QTextEdit();
-    this->setCentralWidget(this->_text);
+    this->_createGrid();
 
     this->setWindowTitle(tr("Yaydr project manager"));
     this->setWindowIcon(QIcon(":/images/yaydr.svg"));
     this->resize(800,600);
+}
+void ProjectManagerWindow::_createGrid()
+{
+    /* Initialize the scrollarea */
+    this->_viewportScrollArea = new QScrollArea();
+    this->_viewportScrollArea->setWidgetResizable(true);
+    this->_viewportScrollArea->setVerticalScrollBarPolicy(Qt::ScrollBarAsNeeded);
+
+    /* Initialze the Vertical layout */
+    this->_mainGrid = new QVBoxLayout(this->_viewportScrollArea);
+    this->_mainGrid->setSizeConstraint(QLayout::SetNoConstraint);
+
+    /* Initialize the scrollable widget */
+    this->_scrollWidget = new QWidget();
+    this->_scrollWidget->setLayout(this->_mainGrid);
+
+    /* Add the widget to the scrollarea */
+    this->_viewportScrollArea->setWidget(this->_scrollWidget);
+
+    this->setCentralWidget(this->_viewportScrollArea);
 }
 void ProjectManagerWindow::_createMenus()
 {
